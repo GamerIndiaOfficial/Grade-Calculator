f5a731eab0a8a5782ffaf62a453bd2ade932bcd3
GamerIndia <155443275+GamerIndiaOfficial@users.noreply.github.com>
2024-08-10 12:23:11 +0530

    update grade_calculator.py

--- grade_calculator.py
+++ grade_calculator.py
@@ -3,7 +3,7 @@ subject = input("Enter the name of the subject: ")
 max = int(input("Enter the maximum marks: "))
 marks = int(input("Enter the marks you got: ")) #Int is used for whole numbers
 percentage = (marks/max)*100
-round(percentage,2) #rounding off to 2 decimal places 
+percentage = round(percentage,2) #rounding off to 2 decimal places 
 if percentage >= 90:
   print("You got",percentage,"% in",subject,"which is an A+")  
 elif percentage >= 80:
@@ -15,8 +15,8 @@ elif percentage >= 60:
 elif percentage >= 50:
   print("You got",percentage,"% in",subject,"which is a D")
 elif percentage >= 40:
-  print("You got",percentage,"% in",subject,"which is a U")
+  print("You got",percentage,"% in",subject,"which is an U")
 elif percentage < 40:
-  print("Ungraded") #You are ungraded
+  print("You got",percentage,"% in",subject,"which is an Ungraded") #You are ungraded
 else:
-  print("I don't know what you got") 
+  print("I don't know what you got")
